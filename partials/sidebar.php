0a9ab470685e2bca5852849e48bab37e616718c8
insanz01 <insankamil002@gmail.com>
2023-07-06 19:47:11 +0700

    ganti nama aplikasi

--- partials/sidebar.php
+++ partials/sidebar.php
@@ -15,7 +15,7 @@
       <!-- Brand Logo -->
       <a href="index3.html" class="brand-link">
         <img src="dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
-        <span class="brand-text font-weight-light">DEMO PROGRESS</span>
+        <span class="brand-text font-weight-light">DP3K</span>
       </a>
     <?php endif; ?>
 
