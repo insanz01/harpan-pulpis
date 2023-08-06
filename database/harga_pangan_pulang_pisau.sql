7768fb09f33fb42af9ae6fbef56d3456b73a5057
insanz01 <insankamil002@gmail.com>
2023-08-06 19:32:05 +0700

    add db

--- database/harga_pangan_pulang_pisau.sql
+++ database/harga_pangan_pulang_pisau.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 06 Jul 2023 pada 15.27
+-- Waktu pembuatan: 06 Agu 2023 pada 14.31
 -- Versi server: 10.4.27-MariaDB
 -- Versi PHP: 7.4.33
 
@@ -18,8 +18,30 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `harga_pangan`
+-- Database: `harga_pangan_pulang_pisau`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `agenda_pasar_murah`
+--
+
+CREATE TABLE `agenda_pasar_murah` (
+  `id` int(11) NOT NULL,
+  `lokasi` varchar(255) NOT NULL,
+  `tanggal` date NOT NULL,
+  `created_at` datetime NOT NULL DEFAULT current_timestamp(),
+  `updated_at` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `agenda_pasar_murah`
+--
+
+INSERT INTO `agenda_pasar_murah` (`id`, `lokasi`, `tanggal`, `created_at`, `updated_at`) VALUES
+(1, 'Pasar Sudimampir', '2023-07-27', '2023-07-27 07:50:55', '2023-07-27 07:50:55'),
+(2, 'Pasar Malam', '2023-07-30', '2023-07-30 13:08:26', '2023-07-30 13:08:26');
 
 -- --------------------------------------------------------
 
@@ -36,6 +58,13 @@ CREATE TABLE `harga_distributor` (
   `updated_at` datetime DEFAULT current_timestamp(),
   `deleted_at` datetime DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `harga_distributor`
+--
+
+INSERT INTO `harga_distributor` (`id`, `id_komoditas`, `harga`, `approved_at`, `created_at`, `updated_at`, `deleted_at`) VALUES
+(1, 1, 25000, '2023-07-21 02:43:22', '2023-07-21 07:40:02', '2023-07-21 07:40:02', NULL);
 
 -- --------------------------------------------------------
 
@@ -58,10 +87,12 @@ CREATE TABLE `harga_eceran` (
 --
 
 INSERT INTO `harga_eceran` (`id`, `id_komoditas`, `harga`, `approved_at`, `created_at`, `updated_at`, `deleted_at`) VALUES
-(1, 1, 12000, NULL, '2023-06-15 18:24:04', '2023-06-15 18:24:04', NULL),
-(2, 2, 1500, NULL, '2023-06-15 18:57:36', '2023-06-15 18:57:36', NULL),
-(3, 2, 12000, NULL, '2023-06-25 17:11:13', '2023-06-25 17:11:13', '2023-06-25 12:11:21'),
-(4, 1, 1000, NULL, '2023-06-25 17:13:11', '2023-06-25 17:13:11', '2023-06-25 12:13:17');
+(1, 1, 12000, '2023-07-21 02:35:29', '2023-06-15 18:24:04', '2023-07-30 00:00:00', NULL),
+(2, 2, 1500, '2023-08-04 03:54:46', '2023-06-15 18:57:36', '2023-06-15 18:57:36', NULL),
+(3, 2, 12000, '2023-08-04 03:54:46', '2023-06-25 17:11:13', '2023-06-25 17:11:13', '2023-06-25 12:11:21'),
+(4, 1, 1000, '2023-08-04 03:54:46', '2023-06-25 17:13:11', '2023-06-25 17:13:11', '2023-06-25 12:13:17'),
+(5, 3, 500, '2023-08-04 03:54:46', '2023-08-04 08:54:17', '2023-08-04 08:54:17', NULL),
+(6, 6, 100, '2023-08-04 10:18:03', '2023-01-01 00:00:00', '2023-08-04 15:17:48', NULL);
 
 -- --------------------------------------------------------
 
@@ -73,6 +104,7 @@ CREATE TABLE `harga_grosir` (
   `id` int(11) NOT NULL,
   `id_komoditas` int(11) DEFAULT NULL,
   `harga` int(11) DEFAULT NULL,
+  `approved_at` datetime DEFAULT NULL,
   `created_at` datetime DEFAULT current_timestamp(),
   `updated_at` datetime DEFAULT current_timestamp(),
   `deleted_at` datetime DEFAULT NULL
@@ -82,8 +114,10 @@ CREATE TABLE `harga_grosir` (
 -- Dumping data untuk tabel `harga_grosir`
 --
 
-INSERT INTO `harga_grosir` (`id`, `id_komoditas`, `harga`, `created_at`, `updated_at`, `deleted_at`) VALUES
-(1, 1, 9000, '2023-06-15 18:59:04', '2023-06-15 18:59:04', NULL);
+INSERT INTO `harga_grosir` (`id`, `id_komoditas`, `harga`, `approved_at`, `created_at`, `updated_at`, `deleted_at`) VALUES
+(1, 1, 9000, '2023-08-04 04:08:26', '2023-06-15 18:59:04', '2023-06-15 18:59:04', NULL),
+(2, 6, 125, '2023-08-04 04:08:26', '2023-08-04 09:04:13', '2023-08-04 09:04:13', NULL),
+(3, 3, 200, NULL, '2023-08-04 11:36:32', '2023-08-04 11:36:32', NULL);
 
 -- --------------------------------------------------------
 
@@ -106,7 +140,9 @@ CREATE TABLE `harga_nasional` (
 --
 
 INSERT INTO `harga_nasional` (`id`, `id_komoditas`, `harga`, `approved_at`, `created_at`, `updated_at`, `deleted_at`) VALUES
-(1, 1, 12000, NULL, '2023-07-06 20:12:32', '2023-07-06 20:12:32', NULL);
+(1, 1, 12000, '2023-08-01 10:08:13', '2023-07-06 20:12:32', '2023-07-06 20:12:32', NULL),
+(2, 3, 4000, '2023-08-01 10:08:13', '2023-07-30 20:59:33', '2023-07-30 00:00:00', NULL),
+(3, 1, 9500, '2023-08-01 10:08:59', '2023-08-01 15:08:45', '2023-08-01 15:08:45', NULL);
 
 -- --------------------------------------------------------
 
@@ -127,6 +163,34 @@ CREATE TABLE `harga_produsen` (
 -- --------------------------------------------------------
 
 --
+-- Struktur dari tabel `harga_sembako`
+--
+
+CREATE TABLE `harga_sembako` (
+  `id` int(11) NOT NULL,
+  `id_sembako` int(11) NOT NULL,
+  `id_komoditas` int(11) NOT NULL,
+  `satuan` varchar(100) NOT NULL,
+  `harga_pedagang_1` int(11) NOT NULL,
+  `harga_pedagang_2` int(11) NOT NULL,
+  `harga_pedagang_3` int(11) NOT NULL,
+  `harga_pedagang_4` int(11) NOT NULL,
+  `created_at` datetime NOT NULL DEFAULT current_timestamp(),
+  `updated_at` datetime NOT NULL DEFAULT current_timestamp(),
+  `deleted_at` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `harga_sembako`
+--
+
+INSERT INTO `harga_sembako` (`id`, `id_sembako`, `id_komoditas`, `satuan`, `harga_pedagang_1`, `harga_pedagang_2`, `harga_pedagang_3`, `harga_pedagang_4`, `created_at`, `updated_at`, `deleted_at`) VALUES
+(1, 1, 1, 'Kg', 9000, 9500, 9000, 9200, '2023-08-06 17:57:14', '2023-08-06 17:57:14', NULL),
+(2, 1, 2, 'Butir', 2000, 1000, 1500, 1200, '2023-08-06 17:57:51', '2023-08-06 17:57:51', NULL);
+
+-- --------------------------------------------------------
+
+--
 -- Struktur dari tabel `inflasi`
 --
 
@@ -135,7 +199,7 @@ CREATE TABLE `inflasi` (
   `id_permintaan` int(11) DEFAULT NULL,
   `nominal` int(11) DEFAULT NULL,
   `nilai` varchar(20) DEFAULT NULL,
-  `approved_at` datetime DEFAULT NULL,
+  `approved_at` datetime DEFAULT current_timestamp(),
   `created_at` datetime DEFAULT current_timestamp(),
   `updated_at` datetime DEFAULT current_timestamp(),
   `deleted_at` datetime DEFAULT NULL
@@ -146,7 +210,31 @@ CREATE TABLE `inflasi` (
 --
 
 INSERT INTO `inflasi` (`id`, `id_permintaan`, `nominal`, `nilai`, `approved_at`, `created_at`, `updated_at`, `deleted_at`) VALUES
-(4, 1, 6, 'Naik Rendah', NULL, '2023-07-06 20:09:11', '2023-07-06 20:09:11', NULL);
+(4, 1, 5, 'Naik Rendah', '2023-07-06 20:09:11', '2023-07-06 20:09:11', '2023-07-30 00:00:00', NULL),
+(5, 2, 2, 'Naik Rendah', '2023-08-04 08:53:31', '2023-08-04 08:53:31', '2023-08-04 08:53:31', NULL),
+(6, 2, 8, 'Naik Rendah', '2023-08-04 11:26:31', '2023-08-04 11:26:31', '2023-08-04 11:26:31', NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `kategori_barang`
+--
+
+CREATE TABLE `kategori_barang` (
+  `id` int(11) NOT NULL,
+  `nama` varchar(150) NOT NULL,
+  `created_at` datetime NOT NULL DEFAULT current_timestamp(),
+  `updated_at` datetime NOT NULL DEFAULT current_timestamp(),
+  `deleted_at` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `kategori_barang`
+--
+
+INSERT INTO `kategori_barang` (`id`, `nama`, `created_at`, `updated_at`, `deleted_at`) VALUES
+(1, 'Minyak', '2023-08-06 15:44:42', '2023-08-06 15:44:42', NULL),
+(2, 'Beras', '2023-08-06 15:45:44', '2023-08-06 15:45:44', NULL);
 
 -- --------------------------------------------------------
 
@@ -157,8 +245,12 @@ INSERT INTO `inflasi` (`id`, `id_permintaan`, `nominal`, `nilai`, `approved_at`,
 CREATE TABLE `komoditas` (
   `id` int(11) NOT NULL,
   `nama` varchar(100) DEFAULT NULL,
-  `id_satuan` int(11) DEFAULT NULL,
-  `approved_at` datetime DEFAULT NULL,
+  `id_satuan` int(11) DEFAULT 4,
+  `satuan` varchar(50) NOT NULL,
+  `kategori_id` int(11) NOT NULL,
+  `kategori` varchar(100) NOT NULL,
+  `merk` varchar(200) NOT NULL,
+  `approved_at` datetime DEFAULT current_timestamp(),
   `created_at` datetime DEFAULT current_timestamp(),
   `updated_at` datetime DEFAULT current_timestamp(),
   `deleted_at` datetime DEFAULT NULL
@@ -168,9 +260,12 @@ CREATE TABLE `komoditas` (
 -- Dumping data untuk tabel `komoditas`
 --
 
-INSERT INTO `komoditas` (`id`, `nama`, `id_satuan`, `approved_at`, `created_at`, `updated_at`, `deleted_at`) VALUES
-(1, 'Beras', 1, NULL, '2023-06-15 10:45:16', '2023-06-15 10:45:16', NULL),
-(2, 'Telur', 3, NULL, '2023-06-15 10:46:04', '2023-06-15 10:46:04', NULL);
+INSERT INTO `komoditas` (`id`, `nama`, `id_satuan`, `satuan`, `kategori_id`, `kategori`, `merk`, `approved_at`, `created_at`, `updated_at`, `deleted_at`) VALUES
+(1, 'Beras', 1, 'Kg', 0, '', '', '2023-06-15 10:45:16', '2023-06-15 10:45:16', '2023-06-15 10:45:16', NULL),
+(2, 'Telur', 3, 'Butir', 0, '', '', '2023-06-15 10:46:04', '2023-06-15 10:46:04', '2023-06-15 10:46:04', NULL),
+(3, 'Teh', 2, 'Kotak', 0, '', '', '2023-07-26 11:01:13', '2023-07-26 11:01:13', '2023-07-30 00:00:00', NULL),
+(6, 'Kopi', 4, 'Kg', 0, '', '', '2023-07-30 19:01:34', '2023-07-30 00:00:00', '2023-07-30 00:00:00', NULL),
+(7, 'Teh', 4, 'Kotak', 0, '', '', '2023-07-30 19:02:31', '2023-07-30 00:00:00', '2023-07-30 00:00:00', '2023-07-30 14:05:41');
 
 -- --------------------------------------------------------
 
@@ -185,6 +280,38 @@ CREATE TABLE `kritik_saran` (
   `updated_at` datetime NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Dumping data untuk tabel `kritik_saran`
+--
+
+INSERT INTO `kritik_saran` (`id`, `pesan`, `created_at`, `updated_at`) VALUES
+(1, 'percobaan kami', '2023-07-21 07:26:55', '2023-07-21 07:26:55'),
+(2, 'ini adalah kritik dan saran yang baru', '2023-07-26 10:16:00', '2023-07-26 10:16:00');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `pasar`
+--
+
+CREATE TABLE `pasar` (
+  `id` int(11) NOT NULL,
+  `nama` varchar(150) NOT NULL,
+  `kecamatan` varchar(200) NOT NULL,
+  `kelurahan` varchar(200) NOT NULL,
+  `keterangan` text NOT NULL,
+  `created_at` datetime NOT NULL DEFAULT current_timestamp(),
+  `updated_at` datetime NOT NULL DEFAULT current_timestamp(),
+  `deleted_at` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `pasar`
+--
+
+INSERT INTO `pasar` (`id`, `nama`, `kecamatan`, `kelurahan`, `keterangan`, `created_at`, `updated_at`, `deleted_at`) VALUES
+(1, 'Pasar Percontohan', 'Banjarmasin Barat', 'Telagabiru', 'pasar ini hanya fiktif semata', '2023-08-06 17:13:41', '2023-08-06 17:13:41', NULL);
+
 -- --------------------------------------------------------
 
 --
@@ -195,7 +322,7 @@ CREATE TABLE `permintaan` (
   `id` int(11) NOT NULL,
   `id_komoditas` int(11) DEFAULT NULL,
   `jumlah` int(11) DEFAULT NULL,
-  `approved_at` datetime DEFAULT NULL,
+  `approved_at` datetime DEFAULT current_timestamp(),
   `created_at` datetime DEFAULT current_timestamp(),
   `updated_at` datetime DEFAULT current_timestamp(),
   `deleted_at` datetime DEFAULT NULL
@@ -206,7 +333,9 @@ CREATE TABLE `permintaan` (
 --
 
 INSERT INTO `permintaan` (`id`, `id_komoditas`, `jumlah`, `approved_at`, `created_at`, `updated_at`, `deleted_at`) VALUES
-(1, 1, 100, NULL, '2023-07-06 20:02:09', '2023-07-06 20:02:09', NULL);
+(1, 1, 10, '2023-07-06 20:02:09', '2023-08-04 00:00:00', '2023-08-04 00:00:00', NULL),
+(2, 3, 2, '2023-07-30 21:10:54', '2023-07-30 21:10:54', '2023-07-30 21:10:54', '2023-07-30 16:11:52'),
+(3, 2, 100, '2023-08-04 20:02:58', '2023-08-03 00:00:00', '2023-08-04 20:02:58', NULL);
 
 -- --------------------------------------------------------
 
@@ -245,7 +374,31 @@ CREATE TABLE `satuan` (
 INSERT INTO `satuan` (`id`, `nama`) VALUES
 (1, 'Kg'),
 (2, 'Pc'),
-(3, 'Butir');
+(3, 'Butir'),
+(4, 'Lainnya');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `sembako`
+--
+
+CREATE TABLE `sembako` (
+  `id` int(11) NOT NULL,
+  `id_pasar` int(11) NOT NULL,
+  `petugas` varchar(255) NOT NULL,
+  `approved_at` datetime DEFAULT NULL,
+  `created_at` datetime NOT NULL DEFAULT current_timestamp(),
+  `updated_at` datetime NOT NULL DEFAULT current_timestamp(),
+  `deleted_at` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `sembako`
+--
+
+INSERT INTO `sembako` (`id`, `id_pasar`, `petugas`, `approved_at`, `created_at`, `updated_at`, `deleted_at`) VALUES
+(1, 1, 'Palui; Waluh', NULL, '2023-08-06 17:55:59', '2023-08-06 17:55:59', NULL);
 
 -- --------------------------------------------------------
 
@@ -257,7 +410,7 @@ CREATE TABLE `stok_komoditas` (
   `id` int(11) NOT NULL,
   `id_komoditas` int(11) DEFAULT NULL,
   `stok` int(11) DEFAULT NULL,
-  `approved_at` datetime DEFAULT NULL,
+  `approved_at` datetime DEFAULT current_timestamp(),
   `created_at` datetime DEFAULT current_timestamp(),
   `updated_at` datetime DEFAULT current_timestamp(),
   `deleted_at` datetime DEFAULT NULL
@@ -268,7 +421,8 @@ CREATE TABLE `stok_komoditas` (
 --
 
 INSERT INTO `stok_komoditas` (`id`, `id_komoditas`, `stok`, `approved_at`, `created_at`, `updated_at`, `deleted_at`) VALUES
-(1, 1, 5, '2023-06-25 12:48:39', '2023-06-25 17:42:11', '2023-06-25 17:42:11', NULL);
+(1, 1, 5, '2023-06-25 12:48:39', '2023-06-25 17:42:11', '2023-06-25 17:42:11', NULL),
+(2, 3, 1000, '2023-08-04 08:51:01', '2023-08-04 08:51:01', '2023-08-04 08:51:01', NULL);
 
 -- --------------------------------------------------------
 
@@ -281,20 +435,28 @@ CREATE TABLE `user` (
   `nama` varchar(100) DEFAULT NULL,
   `username` varchar(100) DEFAULT NULL,
   `password` varchar(100) DEFAULT NULL,
-  `id_role` int(11) DEFAULT NULL
+  `id_role` int(11) DEFAULT NULL,
+  `aktif` tinyint(1) NOT NULL DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data untuk tabel `user`
 --
 
-INSERT INTO `user` (`id`, `nama`, `username`, `password`, `id_role`) VALUES
-(2, 'Pimpinan', '12345', '$2y$10$6ZhDHCtKLTwrmFxnVa9WRelhOwGNHK5t2TcVdj2h2o1THSv2xTapu', 1),
-(3, 'Administrator Biasa', '54321', '$2y$10$6ZhDHCtKLTwrmFxnVa9WRelhOwGNHK5t2TcVdj2h2o1THSv2xTapu', 2);
+INSERT INTO `user` (`id`, `nama`, `username`, `password`, `id_role`, `aktif`) VALUES
+(2, 'Pimpinan', '12345', '$2y$10$6ZhDHCtKLTwrmFxnVa9WRelhOwGNHK5t2TcVdj2h2o1THSv2xTapu', 1, 1),
+(3, 'Administrator Biasa', '54321', '$2y$10$6ZhDHCtKLTwrmFxnVa9WRelhOwGNHK5t2TcVdj2h2o1THSv2xTapu', 2, 1),
+(4, 'Insan Kamil', 'insanz01', '$2y$10$cF6Jt/XXZtJmW3gTusBFOOafanoZj61GVjMnDYCI5z3st0vW4OVuu', 2, 1);
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indeks untuk tabel `agenda_pasar_murah`
+--
+ALTER TABLE `agenda_pasar_murah`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indeks untuk tabel `harga_distributor`
@@ -332,11 +494,23 @@ ALTER TABLE `harga_produsen`
   ADD KEY `harga_produsen_ibfk_1` (`id_komoditas`);
 
 --
+-- Indeks untuk tabel `harga_sembako`
+--
+ALTER TABLE `harga_sembako`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indeks untuk tabel `inflasi`
 --
 ALTER TABLE `inflasi`
   ADD PRIMARY KEY (`id`),
   ADD KEY `inflasi_ibfk_1` (`id_permintaan`);
+
+--
+-- Indeks untuk tabel `kategori_barang`
+--
+ALTER TABLE `kategori_barang`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indeks untuk tabel `komoditas`
@@ -349,6 +523,12 @@ ALTER TABLE `komoditas`
 -- Indeks untuk tabel `kritik_saran`
 --
 ALTER TABLE `kritik_saran`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks untuk tabel `pasar`
+--
+ALTER TABLE `pasar`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -371,6 +551,12 @@ ALTER TABLE `satuan`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indeks untuk tabel `sembako`
+--
+ALTER TABLE `sembako`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indeks untuk tabel `stok_komoditas`
 --
 ALTER TABLE `stok_komoditas`
@@ -389,28 +575,34 @@ ALTER TABLE `user`
 --
 
 --
+-- AUTO_INCREMENT untuk tabel `agenda_pasar_murah`
+--
+ALTER TABLE `agenda_pasar_murah`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
 -- AUTO_INCREMENT untuk tabel `harga_distributor`
 --
 ALTER TABLE `harga_distributor`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT untuk tabel `harga_eceran`
 --
 ALTER TABLE `harga_eceran`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT untuk tabel `harga_grosir`
 --
 ALTER TABLE `harga_grosir`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT untuk tabel `harga_nasional`
 --
 ALTER TABLE `harga_nasional`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT untuk tabel `harga_produsen`
@@ -419,28 +611,46 @@ ALTER TABLE `harga_produsen`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
+-- AUTO_INCREMENT untuk tabel `harga_sembako`
+--
+ALTER TABLE `harga_sembako`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
 -- AUTO_INCREMENT untuk tabel `inflasi`
 --
 ALTER TABLE `inflasi`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT untuk tabel `kategori_barang`
+--
+ALTER TABLE `kategori_barang`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT untuk tabel `komoditas`
 --
 ALTER TABLE `komoditas`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT untuk tabel `kritik_saran`
 --
 ALTER TABLE `kritik_saran`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT untuk tabel `pasar`
+--
+ALTER TABLE `pasar`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT untuk tabel `permintaan`
 --
 ALTER TABLE `permintaan`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT untuk tabel `role_user`
@@ -452,19 +662,25 @@ ALTER TABLE `role_user`
 -- AUTO_INCREMENT untuk tabel `satuan`
 --
 ALTER TABLE `satuan`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT untuk tabel `sembako`
+--
+ALTER TABLE `sembako`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT untuk tabel `stok_komoditas`
 --
 ALTER TABLE `stok_komoditas`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT untuk tabel `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
