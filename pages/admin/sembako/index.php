8232a2f863b693b048bb9885804305c54bf4d6bd
insanz01 <insankamil002@gmail.com>
2023-08-17 17:16:47 +0700

    Fix title data pasar

--- pages/admin/sembako/index.php
+++ pages/admin/sembako/index.php
@@ -12,12 +12,12 @@
   <div class="container-fluid">
     <div class="row mb-2">
       <div class="col-sm-6">
-        <h1 class="m-0">Data Sembako</h1>
+        <h1 class="m-0">Pasar yang sudah dimonitoring</h1>
       </div><!-- /.col -->
       <div class="col-sm-6">
         <ol class="breadcrumb float-sm-right">
-          <li class="breadcrumb-item"><a href="#">Sembako</a></li>
-          <li class="breadcrumb-item active">Sembako</li>
+          <li class="breadcrumb-item"><a href="#"></a></li>
+          <li class="breadcrumb-item active"></li>
         </ol>
       </div><!-- /.col -->
     </div><!-- /.row -->
