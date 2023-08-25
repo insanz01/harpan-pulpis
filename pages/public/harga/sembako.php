5471d4a1267829512f18571da8390d2bdd4c21d4
insanz01 <insankamil002@gmail.com>
2023-08-26 00:10:05 +0700

    fix harga pasar in public

--- pages/public/harga/sembako.php
+++ pages/public/harga/sembako.php
@@ -61,7 +61,7 @@
                     <td><?= $datum['petugas'] ?></td>
                     <td><?= $datum['created_at'] ?></td>
                     <td>
-                      <a href="?page=sembako-detail-publik&id=<?= $datum['id'] ?>" class="btn btn-info float-right mx-2" role="button">
+                      <a href="?page=sembako-detail-publik&id_sembako=<?= $datum['id'] ?>&id=<?= $datum['id_pasar'] ?>" class="btn btn-info float-right mx-2" role="button">
                         <i class="fas fa-fw fa-book"></i>
                         Detail
                       </a>
