5c3ee4b71bba2a59ec0b54b9fb71a6008550521e
insanz01 <insankamil002@gmail.com>
2023-09-04 12:03:36 +0700

    change db config

--- database/db.php
+++ database/db.php
@@ -2,7 +2,7 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "harga_pangan_pulang_pisau";
+$dbname = "another_harga_pangan_pulang_pisau";
 
 $connection = mysqli_connect($servername, $username, $password, $dbname);
 if (!$connection) {
